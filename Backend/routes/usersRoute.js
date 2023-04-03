9ee161c893b78d88fc8f5772c33391951b35a8a1
Yasin051197 <yasinjamadar0511@gmail.com>
2023-04-03 13:21:38 +0530

    added files in signin

--- Backend/routes/usersRoute.js
+++ Backend/routes/usersRoute.js
@@ -47,16 +47,39 @@ usersRoute.post("/login",async(req,res)=>{
         const user=await User.find({email:email});
         
         if(user.length>0){
-            bcrypt.compare(password, user[0].password, function(err,result) {
+            if(user[0].login_attempts<=5 && user[0].lock<=24){
+
+            bcrypt.compare(password, user[0].password, async(err,result)=> {
                 if(result){
+                    await User.findByIdAndUpdate(user[0]._id,{login_attempts:0,lock:null})
                     res.send({msg:"Login Successful",email:user[0].email})
                 }
                 else{
-                    res.send({msg:"Login Failed, plz enter correct credintials"})
+
+                    await User.findByIdAndUpdate(user[0]._id,{login_attempts:user[0].login_attempts+1})
+            
+                    if(user[0].login_attempts>=5){
+                        const d= 1000*60*60*24;
+            
+                        const lt=new Date().getTime() + d;
+            
+                        await User.findByIdAndUpdate(user[0]._id,{lock:new Date(lock)});   
+                    }
+                    res.send({msg:"Login Failed, plz enter correct credintials"});
                 }
             });
         }
+        else{
+            let time=24;
+            await User.findByIdAndUpdate(user[0]._id,{lock:time})
+
+            res.send({msg:`U have been blocked for 24 hours} `})
+        }
     }
+    else{
+        res.send({msg:"U r not user please login first"})
+    }
+}
     catch(err){
         res.send(err);
     }
